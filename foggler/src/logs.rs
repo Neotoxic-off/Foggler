e1afda126bc1bc5d0bd3c7e4d2c5bd1952613bf0
Neotoxic-off <neotoxic.off@gmail.com>
2025-04-22 23:57:24 +0200

    update log format, clean version comming

--- foggler/src/logs.rs
+++ foggler/src/logs.rs
@@ -4,6 +4,7 @@ use chrono::Local;
 use colored::*;
 use std::fs;
 use std::path::Path;
+use regex;
 
 fn ensure_logs_folder_exists(folder_path: &str) -> std::io::Result<()> {
     let path = Path::new(folder_path);
@@ -39,13 +40,32 @@ pub fn init(debug: bool, path: &str) -> Result<(), std::io::Error> {
 
     let file_formatter = |out: fern::FormatCallback, message: &std::fmt::Arguments, record: &log::Record| {
         out.finish(format_args!(
-            "[{} {} {}] {}",
+            "{{\"timestamp\":\"{}\",\"level\":\"{}\",\"target\":\"{}\",\"region\":\"{}\",\"latency\":{} }}",
             Local::now().format("%Y-%m-%d %H:%M:%S"),
             record.level(),
             record.target(),
-            message
+            extract_region(message),
+            extract_latency(message)
         ))
     };
+
+    fn extract_region(message: &std::fmt::Arguments) -> String {
+        let regex = regex::Regex::new(r"([A-Za-z0-9 ]+)\.{2,}").unwrap();
+        if let Some(caps) = regex.captures(message.to_string().as_str()) {
+            caps.get(1).map_or_else(|| String::from("Unknown"), |m| m.as_str().to_string())
+        } else {
+            String::from("Unknown")
+        }
+    }
+
+    fn extract_latency(message: &std::fmt::Arguments) -> u32 {
+        let regex = regex::Regex::new(r"(\d+)ms").unwrap();
+        if let Some(caps) = regex.captures(message.to_string().as_str()) {
+            caps.get(1).map_or_else(|| 0, |m| m.as_str().parse().unwrap_or(0))
+        } else {
+            0
+        }
+    }
 
     match Dispatch::new()
         .level(log_level)
