5711690d473e49c840304a1e6b3c427d6be22607
neo <neotoxic.off@gmail.com>
2025-04-25 01:16:00 +0200

    Update ping.rs

--- foggler/src/checker/ping.rs
+++ foggler/src/checker/ping.rs
@@ -9,7 +9,7 @@ pub async fn ping(host: &str, port: u16, timeout_secs: u64) -> Result<u64, u64> 
 
     match timeout(timeout_duration, TcpStream::connect(&address)).await {
         Ok(Ok(_)) => Ok(start.elapsed().as_millis() as u64),
-        Ok(Err(e)) => Err(-1),
-        Err(_) => Err(-1),
+        Ok(Err(e)) => Err(0),
+        Err(_) => Err(0),
     }
 }
